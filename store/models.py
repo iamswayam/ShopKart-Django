71446e8db87b9a8c75c337d5e106e04511c01c30
Swayam <swayamsiddhap@gmail.com>
2021-06-13 18:39:29 +0530

    first commit

---
+++ store/models.py
@@ -0,0 +1,24 @@
+from category.admin import CategoryAdmin
+from django.db import models
+from django.db.models.fields.files import ImageField
+from category.models import Category
+from django.urls import reverse
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    description = models.TextField(max_length=200, blank=True)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    images = models.ImageField(upload_to='photos/products')
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+
+    def __str__(self):
+        return self.product_name
